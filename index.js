6c20f2c036b492d1e8b1bafc633b6653ec3d519b
Osvaldo Jimenez <ojimenez@pacific.edu>
2023-01-13 13:33:32 -0800

    added in the sample middleware

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ let tasks = [
     }
 ]
 
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+app.use(requestLogger)
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello COMP227!</h1>')
 })
