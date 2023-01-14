acff2b3d5512395aa0fa3c006f70821735ef00c4
Osvaldo <ojimenez@pacific.edu>
2023-01-14 10:54:14 -0800

    backend now serving frontend of the code

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
 
 let tasks = [
     {
@@ -94,7 +97,7 @@ const unknownEndpoint = (request, response, next) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
