5cb021043c82f662c2fd346e623f7b78fe7606cb
Osvaldo <ojimenez@pacific.edu>
2023-01-14 01:59:44 -0800

    made changes to the task part

--- index.js
+++ index.js
@@ -1,82 +1,98 @@
 const express = require('express')
 const app = express()
 
-let notes = [
+app.use(express.json())
+
+let tasks = [
   {
     id: 1,
-    content: "HTML is easy",
-    date: "2022-01-10T17:30:31.098Z",
+    content: "Wash the dishes",
+    date: "2023-01-10T17:30:31.098Z",
     important: true
   },
   {
     id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-01-10T18:39:34.091Z",
+    content: "Take out the trash",
+    date: "2023-01-10T18:39:34.091Z",
     important: false
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-01-10T19:20:14.298Z",
+    content: "Buy salty snacks",
+    date: "2023-01-10T19:20:14.298Z",
     important: true
   }
 ]
 
-app.use(express.json())
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
 }
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
+app.use(requestLogger)
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
-
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
-
-  notes = notes.concat(note)
-
-  response.json(note)
+app.get('/', (request, response) => {
+  response.send('<h1>Hello COMP227!</h1>')
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/tasks', (request, response) => {
+  response.json(tasks)
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.get('/api/tasks/:id', (request, response) => {
   const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
-
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
-    response.json(note)
+  const task = tasks.find(task => task.id === id);
+  if (task) {
+    response.json(task)
   } else {
     response.status(404).end()
   }
 })
+
+const generateId = () => {
+  const maxId = tasks.length > 0
+      ? Math.max(...tasks.map(t => t.id))
+      : 0
+  return maxId + 1
+}
+
+app.post('/api/tasks', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const task = {
+    id: generateId(),
+    content: body.content,
+    important: body.important || false,
+    date: new Date().toISOString(),
+  }
+
+  tasks = tasks.concat(task)
+
+  response.json(task)
+})
+
+app.delete('/api/tasks/:id', (request, response) => {
+  const id = Number(request.params.id)
+  tasks = tasks.filter(task => task.id !== id)
+
+  response.status(204).end()
+})
+
+const unknownEndpoint = (request, response, next) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+  next()
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
