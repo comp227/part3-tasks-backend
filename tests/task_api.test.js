50d5dfa6c86e33bf5c352ec414f78f8d9d7c49a2
Osvaldo <ojimenez@pacific.edu>
2023-01-18 13:30:43 -0800

    added a few more tests and now hiding console output during tests

--- tests/task_api.test.js
+++ tests/task_api.test.js
@@ -9,6 +9,18 @@ test('tasks are returned as json', async () => {
         .get('/api/tasks')
         .expect(200)
         .expect('Content-Type', /application\/json/);
+}, 100000);
+
+test('there are two tasks', async () => {
+    const response = await api.get('/api/tasks');
+
+    expect(response.body).toHaveLength(2);
+});
+
+test('the first task is about HTTP methods', async () => {
+    const response = await api.get('/api/tasks');
+
+    expect(response.body[0].content).toBe('Wash the dishes');
 });
 
 afterAll(() => {
