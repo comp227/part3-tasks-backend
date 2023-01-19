8f0719179f7ad4a8901110ecd47b9044a3355c4d
Osvaldo <ojimenez@pacific.edu>
2023-01-18 22:29:58 -0800

    changed status to 201 and added in test_helper and used it to restructure the code

--- tests/task_api.test.js
+++ tests/task_api.test.js
@@ -1,30 +1,18 @@
-const mongoose = require('mongoose');
 const supertest = require('supertest');
+const mongoose = require('mongoose');
+const helper = require('./test_helper'); // highlight-line
 const app = require('../app');
-
 const api = supertest(app);
-const Task = require('../models/task');
 
-const initialTasks = [
-    {
-        content: 'Wash the dishes',
-        date: new Date(),
-        important: false,
-    },
-    {
-        content: 'Take out the trash',
-        date: new Date(),
-        important: true,
-    },
-];
+const Task = require('../models/task');
 
 beforeEach(async () => {
     await Task.deleteMany({});
 
-    let taskObject = new Task(initialTasks[0]);
+    let taskObject = new Task(helper.initialTasks[0]); // highlight-line
     await taskObject.save();
 
-    taskObject = new Task(initialTasks[1]);
+    taskObject = new Task(helper.initialTasks[1]); // highlight-line
     await taskObject.save();
 });
 
@@ -33,25 +21,60 @@ test('tasks are returned as json', async () => {
         .get('/api/tasks')
         .expect(200)
         .expect('Content-Type', /application\/json/);
-}, 100000);
+});
 
 test('all tasks are returned', async () => {
     const response = await api.get('/api/tasks');
 
-    expect(response.body).toHaveLength(initialTasks.length); // highlight-line
+    expect(response.body).toHaveLength(helper.initialTasks.length); // highlight-line
 });
 
 test('a specific task is within the returned tasks', async () => {
     const response = await api.get('/api/tasks');
 
-    // highlight-start
     const contents = response.body.map(r => r.content);
 
     expect(contents).toContain(
         'Take out the trash'
     );
-    // highlight-end
 });
+
+test('a valid task can be added ', async () => {
+    const newTask = {
+        content: 'async/await simplifies making async calls',
+        important: true,
+    };
+
+    await api
+        .post('/api/tasks')
+        .send(newTask)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
+
+    const tasksAtEnd = await helper.tasksInDb(); // highlight-line
+    expect(tasksAtEnd).toHaveLength(helper.initialTasks.length + 1); // highlight-line
+
+    const contents = tasksAtEnd.map(t => t.content); // highlight-line
+    expect(contents).toContain(
+        'async/await simplifies making async calls'
+    );
+});
+
+test('task without content is not added', async () => {
+    const newTask = {
+        important: true
+    };
+
+    await api
+        .post('/api/tasks')
+        .send(newTask)
+        .expect(400);
+
+    const tasksAtEnd = await helper.tasksInDb(); // highlight-line
+
+    expect(tasksAtEnd).toHaveLength(helper.initialTasks.length); // highlight-line
+});
+
 afterAll(() => {
     mongoose.connection.close();
 });
---
+++ tests/test_helper.js
@@ -0,0 +1,31 @@
+const Task = require('../models/task');
+
+const initialTasks = [
+    {
+        content: 'Wash the dishes',
+        date: new Date(),
+        important: false
+    },
+    {
+        content: 'Take out the trash',
+        date: new Date(),
+        important: true
+    }
+];
+
+const nonExistingId = async () => {
+    const task = new Task({ content: 'willremovethissoon', date: new Date() });
+    await task.save();
+    await task.remove();
+
+    return task._id.toString();
+};
+
+const tasksInDb = async () => {
+    const tasks = await Task.find({});
+    return tasks.map(task => task.toJSON());
+};
+
+module.exports = {
+    initialTasks, nonExistingId, tasksInDb
+};
