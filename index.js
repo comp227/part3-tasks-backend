2c0c66cf0bfadbdba8294540fb1a78097b347a92
Osvaldo Jimenez <ojimenez@pacific.edu>
2024-02-01 16:02:54 -0800

    fix: update for 2024

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
 const app = express();
+const cors = require("cors");
 
-app.use(express.static('build'));
+const Task = require("./models/task");
+
+app.use(express.static("dist"));
 app.use(express.json());
+app.use(cors());
 
 const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method);
-    console.log('Path:  ', request.path);
-    console.log('Body:  ', request.body);
-    console.log('---');
+    console.log("Method:", request.method);
+    console.log("Path:  ", request.path);
+    console.log("Body:  ", request.body);
+    console.log("---");
     next();
 };
 
 app.use(requestLogger);
 
-app.use(cors());
-const Task = require('./models/task');
-
-app.get('/', (request, response) => {
-    response.send('<h1>Hello COMP227!</h1>');
+app.get("/", (request, response) => {
+    response.send("<h1>Hello COMP227!</h1>");
 });
 
-app.get('/api/tasks', (request, response) => {
+app.get("/api/tasks", (request, response) => {
     Task.find({}).then(tasks => {
         response.json(tasks);
     });
 });
 
-app.get('/api/tasks/:id', (request, response, next) => {
+app.get("/api/tasks/:id", (request, response, next) => {
     Task.findById(request.params.id)
         .then(task => {
             if (task) {
@@ -40,13 +41,13 @@ app.get('/api/tasks/:id', (request, response, next) => {
         .catch(error => next(error));
 });
 
-app.post('/api/tasks', (request, response, next) => {
+app.post("/api/tasks", (request, response, next) => {
     const body = request.body;
 
     const task = new Task({
         content: body.content,
         important: Boolean(body.important) || false,
-        date: new Date(),
+        date: new Date().toISOString(),
     });
 
     task.save()
@@ -56,13 +57,21 @@ app.post('/api/tasks', (request, response, next) => {
         .catch(error => next(error));
 });
 
-app.put('/api/tasks/:id', (request, response, next) => {
+app.delete("/api/tasks/:id", (request, response, next) => {
+    Task.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end();
+        })
+        .catch(error => next(error));
+});
+
+app.put("/api/tasks/:id", (request, response, next) => {
     const { content, important } = request.body;
 
     Task.findByIdAndUpdate(
         request.params.id,
         { content, important },
-        { new: true, runValidators: true, context: 'query' }
+        { new: true, runValidators: true, context: "query" }
     )
         .then(updatedTask => {
             response.json(updatedTask);
@@ -70,34 +79,25 @@ app.put('/api/tasks/:id', (request, response, next) => {
         .catch(error => next(error));
 });
 
-app.delete('/api/tasks/:id', (request, response, next) => {
-    Task.findByIdAndRemove(request.params.id)
-        .then(() => {
-            response.status(204).end();
-        })
-        .catch(error => next(error));
-});
-
-const unknownEndpoint = (request, response, next) => {
-    response.status(404).send({ error: 'unknown endpoint' });
-    next();
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: "unknown endpoint" });
 };
 
+// handler of requests with unknown endpoint
 app.use(unknownEndpoint);
 
 const errorHandler = (error, request, response, next) => {
     console.error(error.message);
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' });
-    } else if (error.name === 'ValidationError') {
+    if (error.name === "CastError") {
+        return response.status(400).send({ error: "malformatted id" });
+    } else if (error.name === "ValidationError") {
         return response.status(400).json({ error: error.message });
     }
 
     next(error);
 };
 
-// this has to be the last loaded middleware.
 app.use(errorHandler);
 
 // eslint-disable-next-line no-undef
--- mongo.js
+++ mongo.js
@@ -1,28 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose").set("strictQuery", true);
 
 if (process.argv.length < 3) {
-    console.log('Please provide the password as an argument: node mongo.js <password>');
+    console.log("give password as an argument");
     process.exit(1);
 }
 
-// eslint-disable-next-line no-undef
 const password = process.argv[2];
 
-const url = `mongodb+srv://comp227:${password}@cluster0.gb6u3el.mongodb.net/taskApp?retryWrites=true&w=majority`;
+const url = `mongodb+srv://comp227:${password}@cluster0.gb6u3el.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const taskSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
+    content: {
+        type: String,
+        minLength: 5,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
     important: Boolean,
 });
 
-const Task = mongoose.model('Task', taskSchema);
-mongoose.set('strictQuery', true);
+const Task = mongoose.model("Task", taskSchema);
 
 mongoose
     .connect(url)
-    .then(() => {
-        console.log('connected');
+    .then((result) => {
+        console.log("connected");
 
         Task.find({}).then(result => {
             result.forEach(task => {
