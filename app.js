bea6aa71246822ca1b9bc10779f7024c47ef8e19
Osvaldo Jimenez <ojimenez@pacific.edu>
2024-02-06 12:30:47 -0800

    fix: updated to 2024

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-const config = require('./utils/config');
-const express = require('express');
-require('express-async-errors');
+const config = require("./utils/config");
+const express = require("express");
+require("express-async-errors");
 const app = express();
-const cors = require('cors');
-const tasksRouter = require('./controllers/tasks');
-const middleware = require('./utils/middleware');
-const logger = require('./utils/logger');
-const mongoose = require('mongoose').set('strictQuery', true);
+const cors = require("cors");
+const tasksRouter = require("./controllers/tasks");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose").set("strictQuery", true);
 
-logger.info('connecting to', config.MONGODB_URI);
+mongoose.set("strictQuery", false);
+
+logger.info("connecting to", config.MONGODB_URI);
 
 mongoose.connect(config.MONGODB_URI)
     .then(() => {
-        logger.info('connected to MongoDB');
+        logger.info("connected to MongoDB");
     })
     .catch((error) => {
-        logger.error('error connecting to MongoDB:', error.message);
+        logger.error("error connecting to MongoDB:", error.message);
     });
 
 app.use(cors());
-app.use(express.static('build'));
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
-app.use('/api/tasks', tasksRouter);
+app.use("/api/tasks", tasksRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- tests/task_api.test.js
+++ tests/task_api.test.js
@@ -1,95 +1,95 @@
-const supertest = require('supertest');
-const mongoose = require('mongoose');
-const helper = require('./test_helper'); // highlight-line
-const app = require('../app');
+const supertest = require("supertest");
+const mongoose = require("mongoose");
+const helper = require("./test_helper");
+const app = require("../app");
 const api = supertest(app);
 
-const Task = require('../models/task');
+const Task = require("../models/task");
 
 beforeEach(async () => {
     await Task.deleteMany({});
+    console.log("cleared");
 
-    for (let task of helper.initialTasks) {
+    for (const task of helper.initialTasks) {
         let taskObject = new Task(task);
         await taskObject.save();
+        console.log("saved");
     }
+    console.log("done");
 });
 
-test('tasks are returned as json', async () => {
+test("tasks are returned as json", async () => {
     await api
-        .get('/api/tasks')
+        .get("/api/tasks")
         .expect(200)
-        .expect('Content-Type', /application\/json/);
+        .expect("Content-Type", /application\/json/);
 });
 
-test('all tasks are returned', async () => {
-    const response = await api.get('/api/tasks');
+test("all tasks are returned", async () => {
+    const response = await api.get("/api/tasks");
 
-    expect(response.body).toHaveLength(helper.initialTasks.length); // highlight-line
+    expect(response.body).toHaveLength(helper.initialTasks.length);
 });
 
-test('a specific task is within the returned tasks', async () => {
-    const response = await api.get('/api/tasks');
+test("a specific task is within the returned tasks", async () => {
+    const response = await api.get("/api/tasks");
 
     const contents = response.body.map(r => r.content);
 
     expect(contents).toContain(
-        'Take out the trash'
+        "Take out the trash"
     );
 });
 
-test('a valid task can be added ', async () => {
+test("a valid task can be added ", async () => {
     const newTask = {
-        content: 'async/await simplifies making async calls',
+        content: "async/await simplifies making async calls",
         important: true,
     };
 
     await api
-        .post('/api/tasks')
+        .post("/api/tasks")
         .send(newTask)
         .expect(201)
-        .expect('Content-Type', /application\/json/);
+        .expect("Content-Type", /application\/json/);
 
-    const tasksAtEnd = await helper.tasksInDb(); // highlight-line
-    expect(tasksAtEnd).toHaveLength(helper.initialTasks.length + 1); // highlight-line
+    const tasksAtEnd = await helper.tasksInDb();
+    expect(tasksAtEnd).toHaveLength(helper.initialTasks.length + 1);
 
-    const contents = tasksAtEnd.map(t => t.content); // highlight-line
+    const contents = tasksAtEnd.map(t => t.content);
     expect(contents).toContain(
-        'async/await simplifies making async calls'
+        "async/await simplifies making async calls"
     );
 });
 
-test('task without content is not added', async () => {
+test("task without content is not added", async () => {
     const newTask = {
         important: true
     };
 
     await api
-        .post('/api/tasks')
+        .post("/api/tasks")
         .send(newTask)
         .expect(400);
 
-    const tasksAtEnd = await helper.tasksInDb(); // highlight-line
+    const tasksAtEnd = await helper.tasksInDb();
 
-    expect(tasksAtEnd).toHaveLength(helper.initialTasks.length); // highlight-line
+    expect(tasksAtEnd).toHaveLength(helper.initialTasks.length);
 });
 
-test('a specific task can be viewed', async () => {
+test("a specific task can be viewed", async () => {
     const tasksAtStart = await helper.tasksInDb();
-
     const taskToView = tasksAtStart[0];
 
     const resultTask = await api
         .get(`/api/tasks/${taskToView.id}`)
         .expect(200)
-        .expect('Content-Type', /application\/json/);
+        .expect("Content-Type", /application\/json/);
 
-    const processedTaskToView = JSON.parse(JSON.stringify(taskToView));
-
-    expect(resultTask.body).toEqual(processedTaskToView);
+    expect(resultTask.body).toEqual(taskToView);
 });
 
-test('a task can be deleted', async () => {
+test("a task can be deleted", async () => {
     const tasksAtStart = await helper.tasksInDb();
     const taskToDelete = tasksAtStart[0];
 
@@ -98,16 +98,14 @@ test('a task can be deleted', async () => {
         .expect(204);
 
     const tasksAtEnd = await helper.tasksInDb();
-
     expect(tasksAtEnd).toHaveLength(
         helper.initialTasks.length - 1
     );
 
     const contents = tasksAtEnd.map(r => r.content);
-
     expect(contents).not.toContain(taskToDelete.content);
 });
 
-afterAll(() => {
-    mongoose.connection.close();
+afterAll(async () => {
+    await mongoose.connection.close();
 });
