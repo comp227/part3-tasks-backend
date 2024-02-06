7a04d8f2622e5ccae1839e7ea171d6524acfda6e
Osvaldo Jimenez <ojimenez@pacific.edu>
2024-02-06 09:45:03 -0800

    fix: update for 2024

--- tests/task_api.test.js
+++ tests/task_api.test.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
-const supertest = require('supertest');
-const app = require('../app');
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
 
 const api = supertest(app);
-const Task = require('../models/task');
+const Task = require("../models/task");
 
 const initialTasks = [
     {
-        content: 'Wash the dishes',
+        content: "Wash the dishes",
         date: new Date(),
         important: false,
     },
     {
-        content: 'Take out the trash',
+        content: "Take out the trash",
         date: new Date(),
         important: true,
     },
@@ -28,30 +28,24 @@ beforeEach(async () => {
     await taskObject.save();
 });
 
-test('tasks are returned as json', async () => {
+test("tasks are returned as json", async () => {
     await api
-        .get('/api/tasks')
+        .get("/api/tasks")
         .expect(200)
-        .expect('Content-Type', /application\/json/);
+        .expect("Content-Type", /application\/json/);
 }, 100000);
 
-test('all tasks are returned', async () => {
-    const response = await api.get('/api/tasks');
-
-    expect(response.body).toHaveLength(initialTasks.length); // highlight-line
+test("all tasks are returned", async () => {
+    const response = await api.get("/api/tasks");
+    expect(response.body).toHaveLength(initialTasks.length);
 });
 
-test('a specific task is within the returned tasks', async () => {
-    const response = await api.get('/api/tasks');
-
-    // highlight-start
+test("a specific task is within the returned tasks", async () => {
+    const response = await api.get("/api/tasks");
     const contents = response.body.map(r => r.content);
-
-    expect(contents).toContain(
-        'Take out the trash'
-    );
-    // highlight-end
+    expect(contents).toContain("Take out the trash");
 });
-afterAll(() => {
-    mongoose.connection.close();
+
+afterAll(async () => {
+    await mongoose.connection.close();
 });
